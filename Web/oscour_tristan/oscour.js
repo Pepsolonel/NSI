d8647979ab9b8394a336542bebfc9c2c8ec40c5e
Pepsolonel <societegeneralechiatt@gmail.com>
2025-01-24 17:25:39 +0100

    new file:   .vscode/._launch.json 	new file:   .vscode/launch.json 	new file:   Web/oscour_tristan/._index.html 	new file:   Web/oscour_tristan/._oscour.js 	new file:   Web/oscour_tristan/._style.css 	modified:   Web/oscour_tristan/index.html 	modified:   Web/oscour_tristan/oscour.js 	modified:   Web/oscour_tristan/style.css

--- Web/oscour_tristan/oscour.js
+++ Web/oscour_tristan/oscour.js
@@ -1,9 +1,19 @@
 function estConscient(){
-    document.getElementById('nom').innerText='Nom'
-    document.getElementById('nomInput').innerHTML='<input type="text">'
+    document.getElementById('nom').innerText='Nom';
+    document.getElementById('nomInput').innerHTML='<input type="text">';
+    document.getElementById('prénom').innerText='Prénom';
+    document.getElementById('prénomInput').innerHTML='<input type="text">';
+    document.getElementById('prénom').innerText='Prénom';
+    document.getElementById('prénomInput').innerHTML='<input type="text">';
+    document.getElementById('naissance').innerText='Date de naissance';
+    document.getElementById('naissanceInput').innerHTML='<input type="date">';
 }
 
 function estInconscient(){
-    document.getElementById('nom').innerText=null
-    document.getElementById('nomInput').innerHTML=null
+    document.getElementById('nom').innerText=null;
+    document.getElementById('nomInput').innerHTML=null;
+    document.getElementById('prénom').innerText=null;
+    document.getElementById('prénomInput').innerHTML=null;
+    document.getElementById('naissance').innerText=null;
+    document.getElementById('naissanceInput').innerHTML=null;
 }
