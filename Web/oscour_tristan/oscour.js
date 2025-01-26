9c216bc9c862c6be50cd068d8c43427688c9d479
Pepsolonel <societegeneralechiatt@gmail.com>
2025-01-26 23:01:29 +0100

    Fin du script pour oscour 	modified:   Web/oscour_tristan/index.html 	modified:   Web/oscour_tristan/oscour.js 	modified:   Web/oscour_tristan/style.css

--- Web/oscour_tristan/oscour.js
+++ Web/oscour_tristan/oscour.js
@@ -1,12 +1,10 @@
 function estConscient(){
     document.getElementById('nom').innerText='Nom';
-    document.getElementById('nomInput').innerHTML='<input type="text">';
+    document.getElementById('nomInput').innerHTML='<input id="nomValue">';
     document.getElementById('prénom').innerText='Prénom';
-    document.getElementById('prénomInput').innerHTML='<input type="text">';
-    document.getElementById('prénom').innerText='Prénom';
-    document.getElementById('prénomInput').innerHTML='<input type="text">';
+    document.getElementById('prénomInput').innerHTML='<input id="prénomValue">';
     document.getElementById('naissance').innerText='Date de naissance';
-    document.getElementById('naissanceInput').innerHTML='<input type="date">';
+    document.getElementById('naissanceInput').innerHTML='<input type="date" id="naissanceValue">';
 }
 
 function estInconscient(){
@@ -19,23 +17,85 @@ function estInconscient(){
 }
 
 function température(){
-const températureAnormale=document.getElementById('températureAnormale')
+températureAnormale=document.getElementById('températureAnormale');
 
 if(températureAnormale.checked){
-    document.getElementById('températureInput').innerHTML='<input type="number" value="38.5" step="0.5"> °C'
+    document.getElementById('températureInput').innerHTML='<input type="number" id="température" value="38.5" step="0.5"> °C';
 }
 else{
-    document.getElementById('températureInput').innerHTML=null
+    document.getElementById('températureInput').innerHTML=null;
 }
 }
 
 function tension(){
-    const températureAnormale=document.getElementById('tensionAnormale')
+    tensionAnormale=document.getElementById('tensionAnormale');
     
-    if(températureAnormale.checked){
-        document.getElementById('tensionInput').innerHTML='<input type="number" value="141"> mmHg'
+    if(tensionAnormale.checked){
+        document.getElementById('tensionInput').innerHTML='<input type="number" id="tension" value="141"> mmHg';
     }
     else{
-        document.getElementById('tensionInput').innerHTML=null
+        document.getElementById('tensionInput').innerHTML=null;
     }
     }
+
+function valider(){
+    conscienceUnchecked=!document.querySelector('input[name="conscience"][value="conscient"]').checked&!document.querySelector('input[name="conscience"][value="inconscient"]').checked
+    if(conscienceUnchecked){
+        document.getElementById('récapitulatif').innerHTML="<p>Vous n'avez pas tout saisi<p>"
+        return null
+    }
+
+    sexeUnchecked=!document.querySelector('input[name="sexe"][value="masculin"]').checked&!document.querySelector('input[name="sexe"][value="féminin"]').checked
+    if(sexeUnchecked){
+        document.getElementById('récapitulatif').innerHTML="<p>Vous n'avez pas tout saisi<p>"
+        return null
+    }
+
+    conscienceUnchecked=!document.querySelector('input[name="ventilation"][value="spontanée"]').checked&!document.querySelector('input[name="ventilation"][value="mécanique"]').checked
+    if(conscienceUnchecked){
+        document.getElementById('récapitulatif').innerHTML="<p>Vous n'avez pas tout saisi<p>"
+        return null
+    }
+
+    conscience=document.querySelector('input[name="conscience"]:checked').value;
+    if(conscience=='conscient'){
+        nom=document.getElementById('nomValue').value
+        prénom=document.getElementById('prénomValue').value
+        date=document.getElementById('naissanceValue').value
+        a=`<p>Nom : ${nom}</p><p>Prénom : ${prénom}</p><p>Date : ${date}</p>`
+    }
+    else{
+        a=''
+    }
+
+    sexe=document.querySelector('input[name="sexe"]:checked').value
+    b=`<p>Sexe : ${sexe}</p>`
+
+    températureAnormale=document.getElementById('températureAnormale');
+    if(températureAnormale.checked){
+        température=document.getElementById('température').value
+        c=`<p>Température : ${température} °C</p>`
+    }
+    else{
+        c=''
+    }
+
+    tensionAnormale=document.getElementById('tensionAnormale');
+    if(tensionAnormale.checked){
+        tension=document.getElementById('tension').value
+        d=`<p>Tension : ${tension} mmHg</p>`
+    }
+    else{
+        d=''
+    }
+
+    ventilation=document.querySelector('input[name="ventilation"]:checked').value
+    e=`<p>Ventilation : ${ventilation}</p>`
+
+    description=document.getElementById('description').value
+    f=`<p>Description : ${description}</p>`
+
+    récapitulatif='<h2>Récapitulatif :</h2>'+a+b+c+d+e+f
+
+    document.getElementById('récapitulatif').innerHTML=récapitulatif
+}
