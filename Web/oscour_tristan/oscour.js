c713d4c5509c117ffab05e023afaeb3b52dea430
Pepsolonel <societegeneralechiatt@gmail.com>
2025-01-27 10:54:49 +0100

    deleted:    .vscode/._launch.json 	modified:   Web/oscour_tristan/oscour.js

--- Web/oscour_tristan/oscour.js
+++ Web/oscour_tristan/oscour.js
@@ -17,14 +17,14 @@ function estInconscient(){
 }
 
 function température(){
-températureAnormale=document.getElementById('températureAnormale');
+    températureAnormale=document.getElementById('températureAnormale');
 
-if(températureAnormale.checked){
-    document.getElementById('températureInput').innerHTML='<input type="number" id="température" value="38.5" step="0.5"> °C';
-}
-else{
-    document.getElementById('températureInput').innerHTML=null;
-}
+    if(températureAnormale.checked){
+        document.getElementById('températureInput').innerHTML='<input type="number" id="température" value="38.5" step="0.5"> °C';
+    }
+    else{
+        document.getElementById('températureInput').innerHTML=null;
+    }
 }
 
 function tension(){
@@ -39,63 +39,63 @@ function tension(){
     }
 
 function valider(){
-    conscienceUnchecked=!document.querySelector('input[name="conscience"][value="conscient"]').checked&!document.querySelector('input[name="conscience"][value="inconscient"]').checked
+    conscienceUnchecked=!document.querySelector('input[name="conscience"][value="conscient"]').checked&!document.querySelector('input[name="conscience"][value="inconscient"]').checked;
     if(conscienceUnchecked){
-        document.getElementById('récapitulatif').innerHTML="<p>Vous n'avez pas tout saisi<p>"
-        return null
+        document.getElementById('récapitulatif').innerHTML="<p>Vous n'avez pas tout saisi<p>";
+        return null;
     }
 
-    sexeUnchecked=!document.querySelector('input[name="sexe"][value="masculin"]').checked&!document.querySelector('input[name="sexe"][value="féminin"]').checked
+    sexeUnchecked=!document.querySelector('input[name="sexe"][value="masculin"]').checked&!document.querySelector('input[name="sexe"][value="féminin"]').checked;
     if(sexeUnchecked){
-        document.getElementById('récapitulatif').innerHTML="<p>Vous n'avez pas tout saisi<p>"
-        return null
+        document.getElementById('récapitulatif').innerHTML="<p>Vous n'avez pas tout saisi<p>";
+        return null;
     }
 
-    conscienceUnchecked=!document.querySelector('input[name="ventilation"][value="spontanée"]').checked&!document.querySelector('input[name="ventilation"][value="mécanique"]').checked
-    if(conscienceUnchecked){
-        document.getElementById('récapitulatif').innerHTML="<p>Vous n'avez pas tout saisi<p>"
-        return null
+    ventilationUnchecked=!document.querySelector('input[name="ventilation"][value="spontanée"]').checked&!document.querySelector('input[name="ventilation"][value="mécanique"]').checked;
+    if(ventilationUnchecked){
+        document.getElementById('récapitulatif').innerHTML="<p>Vous n'avez pas tout saisi<p>";
+        return null;
     }
 
     conscience=document.querySelector('input[name="conscience"]:checked').value;
     if(conscience=='conscient'){
-        nom=document.getElementById('nomValue').value
-        prénom=document.getElementById('prénomValue').value
-        date=document.getElementById('naissanceValue').value
-        a=`<p>Nom : ${nom}</p><p>Prénom : ${prénom}</p><p>Date : ${date}</p>`
+        nom=document.getElementById('nomValue').value;
+        prénom=document.getElementById('prénomValue').value;
+        date=document.getElementById('naissanceValue').value;
+        a=`<p>État du patient : ${conscience}</p><p>Nom : ${nom}</p><p>Prénom : ${prénom}</p><p>Date : ${date}</p>`;
     }
     else{
-        a=''
+        a=`<p>État du patient : ${conscience}</p>`;
     }
 
-    sexe=document.querySelector('input[name="sexe"]:checked').value
-    b=`<p>Sexe : ${sexe}</p>`
+    sexe=document.querySelector('input[name="sexe"]:checked').value;
+    b=`<p>Sexe : ${sexe}</p>`;
 
     températureAnormale=document.getElementById('températureAnormale');
     if(températureAnormale.checked){
-        température=document.getElementById('température').value
-        c=`<p>Température : ${température} °C</p>`
+        température=document.getElementById('température').value;
+        c=`<p>Température : ${température} °C</p>`;
     }
     else{
-        c=''
+        c='';
     }
 
     tensionAnormale=document.getElementById('tensionAnormale');
     if(tensionAnormale.checked){
-        tension=document.getElementById('tension').value
-        d=`<p>Tension : ${tension} mmHg</p>`
+        tension=document.getElementById('tension').value;
+        d=`<p>Tension : ${tension} mmHg</p>`;
     }
     else{
-        d=''
+        d='';
     }
 
-    ventilation=document.querySelector('input[name="ventilation"]:checked').value
-    e=`<p>Ventilation : ${ventilation}</p>`
+    ventilation=document.querySelector('input[name="ventilation"]:checked').value;
+    e=`<p>Ventilation : ${ventilation}</p>`;
 
-    description=document.getElementById('description').value
-    f=`<p>Description : ${description}</p>`
+    description=document.getElementById('description').value;
+    f=`<p>Description : ${description}</p>`;
 
-    récapitulatif='<h2>Récapitulatif :</h2>'+a+b+c+d+e+f
+    récapitulatif='<h2>Récapitulatif :</h2>'+a+b+c+d+e+f;
 
-    document.getElementById('récapitulatif').innerHTML=récapitulatif
+    document.getElementById('récapitulatif').innerHTML=récapitulatif;
 }
